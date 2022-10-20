459df01c4aec5d7b35c724908be53dcba510e7af
CristianJ77 <cristianfigueroa489@gmail.com>
2022-10-19 18:06:36 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const dbConnect = require('./config/mongo')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static("storage"))
+
+const port = process.env.PORT || 3000
+
+/**
+ * Aqui invocamos a las rutas
+ */
+
+//TODO localhost/api/_____________
+app.use("/api",require("./routes"))
+
+app.listen(port, () => {
+    console.log('Tu app esta lista  por http:localhost:' + port)
+})
+
+
+dbConnect()
